664859ece0506d8364776064aaadfafbac3db95c
Prethish Mathew <pmathew@saturnsys.com>
2016-09-21 21:48:47 -0500

    add try catch to post

--- meetCodes/src/meetCodes/Controllers/meetCodeController.cs
+++ meetCodes/src/meetCodes/Controllers/meetCodeController.cs
@@ -1,29 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using meetCodes.Services.MeetCodeService;
 using MeetCodes.MeetCodeDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace meetCodes.Controllers
 {
     [Route("api/v1/meetcodes")]
     [Produces("application/json")]
-    public class MeetCodeController:Controller
+    public class MeetCodeController : Controller
     {
         private readonly IMeetCodeService _meetCodeService;
+
         public MeetCodeController(IMeetCodeService meetCodeService)
         {
             _meetCodeService = meetCodeService;
         }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
-        {            
+        {
             return Ok(await _meetCodeService.GetMeetCodesAsync(id));
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]MeetCodesDto meetCode)
+        public async Task<IActionResult> Post([FromBody] MeetCodesDto meetCode)
         {
-            return Ok(await _meetCodeService.CreateMeetCodesAsync(meetCode));
+            try
+            {
+                return Ok(await _meetCodeService.CreateMeetCodesAsync(meetCode));
+            }
+            catch (DbUpdateException )
+            {
+                return BadRequest("This meet code is already taken. Please Use another one ");
+            }
+            
         }
     }
 }
